1c364a90137daa8e6e89e153a4758313f46a6180
Jordan Jaramillo <62086742+jordanrjdev@users.noreply.github.com>
2021-11-02 13:45:05 -0500

    feat: agregando la documentacion del repositorio

--- src/graphql/resolver.ts
+++ src/graphql/resolver.ts
@@ -1,5 +1,23 @@
+import { Person } from '../types/person';
+const persons: Array<Person> = [
+  {
+    id: 1,
+    name: 'John Doe',
+    age: 30,
+    email: 'john@correo.com',
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    age: 25,
+  },
+];
 export const resolvers = {
   Query: {
     hello: () => 'Hello world!',
+    getPersons: (): Array<Person> => persons,
+    findPerson: (root: any, args: { id: number }): Person | undefined => {
+      return persons.find((person) => person.id === args.id);
+    },
   },
 };
